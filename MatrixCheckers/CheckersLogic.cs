d3b42b61e279323a03b28461e9b0ac2f8a9bd4e6
josef3113 <33261454+josef3113@users.noreply.github.com>
2018-04-18 12:06:29 +0300

    locate is out struct

--- MatrixCheckers/CheckersLogic.cs
+++ MatrixCheckers/CheckersLogic.cs
@@ -7,28 +7,7 @@ using System.Text;
 namespace MatrixCheckers
 {
     //yosi start
-    public struct Locat
-    {
-        public Locat(int x, int y)
-        {
-            this.X = (byte)x;
-            this.Y = (byte)y;
-
-        }
-        public byte X { get; set; }
-        public byte Y { get; set; }
-
-        private int Compare(Locat i_Other)
-        {
-            int result = 0;
-            if (this.Y > i_Other.Y)
-            {
-                result = 1;
-            }
-            return result;
-        }
-
-    }
+    
     //yosi end
     class CheckersLogic
     {
---
+++ MatrixCheckers/Locat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixCheckers
+{
+    public struct Locat
+    {
+        public Locat(int x, int y)
+        {
+            this.X = (byte)x;
+            this.Y = (byte)y;
+
+        }
+        public byte X { get; set; }
+        public byte Y { get; set; }
+
+        //private int Compare(Locat i_Other)
+        //{
+        //    int result = 0;
+        //    if (this.Y > i_Other.Y)
+        //    {
+        //        result = 1;
+        //    }
+        //    return result;
+        //}
+
+    }
+}
